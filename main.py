f6e0bddf76fe72f146b04801331af770150d1e7e
Nicolas HIOT <nicolas.hiot@etu.univ-orleans.fr>
2017-10-23 13:22:09 +0200

    Change main to python3

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ def main():
     """ main """
 
     if len(sys.argv) < 2:
-        print "Usage: %s <filename>" % sys.argv[0]
+        print("Usage: %s <filename>" % sys.argv[0])
         exit(2)
 
     try:
-        g = graph.io.from_graph(sys.argv[1])
+        g, k = graph.io.from_graph(sys.argv[1])
         if len(sys.argv) > 2:
-            graph.io.to_dot(g[0], sys.argv[2])
+            graph.io.to_dot(g, sys.argv[2])
     except IOError:
         sys.exit("%s: file not exist" % sys.argv[1])
     except graph.io.ParseError:
         sys.exit("%s: not a valid graph file" % sys.argv[1])
 
-    print("yes" if graph.fvs.repeat_random_fvs(g[0], g[1]) else "no")
+    print("yes" if graph.fvs.repeat_random_fvs(g, k) else "no")
 
 if __name__ == '__main__':
     main()
